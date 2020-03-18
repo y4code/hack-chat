fff6fb924df6de3329f7eef00d5ef2f050a883c3
y4code <8910420+y4code@users.noreply.github.com>
2020-03-18 18:29:55 +0800

    test gateway seperating

--- main.go
+++ main.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"html"
 	"log"
 	"net/http"
 )
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
-
-	if r.URL.Path != "/" {
-		http.Error(w, "Not Found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	http.ServeFile(w, r, "home.html")
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	//if r.URL.Path != "/" {
+	//	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	//	//http.Error(w, "Not Found", http.StatusNotFound)
+	//	return
+	//}
+	//if r.Method != "GET" {
+	//	http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	//	return
+	//}
+	//http.ServeFile(w, r, "home.html")
 }
+
 
 func main() {
 	flag.Parse()
