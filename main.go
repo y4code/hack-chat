e3cfab34292f870a420b51e085b9ec992c401f3a
y4code <8910420+y4code@users.noreply.github.com>
2020-03-19 01:08:56 +0800

    test

--- main.go
+++ main.go
@@ -10,21 +10,21 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
 
 	if r.URL.Path != "/" {
-		http.Error(w, "Not Found", http.StatusNotFound)
+		http.Error(w, "无", http.StatusNotFound)
 		return
 	}
 	if r.Method != "GET" {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	http.ServeFile(w, r, "static/home.html")
+	http.ServeFile(w, r, "./static/home.html")
 }
 
 func main() {
 	flag.Parse()
 	hub := newHub()
 	go hub.run()
-	http.HandleFunc("/test", serveHome)
+	http.HandleFunc("/", serveHome)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
